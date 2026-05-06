a9fc1205c87d86a7ea06c30770fd350609bd82df
Vinicius Spalla <89164337+Spalla017@users.noreply.github.com>
2026-05-05 21:50:31 -0300

    Add initial implementation of gulosa class

---
+++ gulosa.py
@@ -0,0 +1,14 @@
+from vertice import Vertice
+from grafo import Grafo
+
+class gulosa:
+    def __init__(self, vertice):
+        self.vertice = vertice
+        self.status = False
+
+    def busca(self, verticeAtual):
+        print("Vertice Atual: ", verticeAtual)
+        if(verticeAtual == self.vertice or self.distancia.Vertice(verticeAtual)):
+            print("Você chegou ao destino!")
+        else:
+            cr
