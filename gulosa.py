e4bd5efdbba993e3a36431bdcda3b703a2ec177c
Vinicius Spalla <89164337+Spalla017@users.noreply.github.com>
2026-05-06 16:48:29 -0300

    Refactor gulosa class and update busca method
    Refactor gulosa class to improve readability and functionality.

--- gulosa.py
+++ gulosa.py
@@ -1,14 +1,25 @@
 from vertice import Vertice
-from grafo import Grafo
 
-class gulosa:
-    def __init__(self, vertice):
-        self.vertice = vertice
-        self.status = False
+class Gulosa:
+    def __init__(self, objetivo):
+        self.objetivo = objetivo
+        self.encontrado = False
 
-    def busca(self, verticeAtual):
-        print("Vertice Atual: ", verticeAtual)
-        if(verticeAtual == self.vertice or self.distancia.Vertice(verticeAtual)):
+    def busca(self, atual):
+
+        print("Vertice Atual: ", atual.rotulo)
+        if atual == self.objetivo or atual.distancia == 0:
+            self.encontrado = True
             print("Você chegou ao destino!")
-        else:
-            cr
+            return
+
+        mlhrAdj = None
+
+        for adj in atual.adjacentes:
+            if not adj.vertice.visitado:
+                if mlhrAdj is None or adj.vertice.distancia < mlhrAdj.vertice.distancia:
+                    mlhrAdj = adj
+
+        if mlhrAdj is not None:
+            mlhrAdj.vertice.visitado = True
+            self.busca(mlhrAdj.vertice)
